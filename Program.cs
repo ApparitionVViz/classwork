d37ddf6bac495e9735a0942dcf0cadcf989f0c9b
Lukashev Roman <74509534+ApparitionVViz@users.noreply.github.com>
2026-05-07 17:52:19 +0300

    classwork0705

--- Program.cs
+++ Program.cs
@@ -1,73 +1,114 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace ConsoleApp1
 {
-    internal class Program
+    public class Program
     {
+        public class Movie
+        {
+            private string _name;
+            private int _duration;
+            private int[] _review;
+            public string Name => _name;
+            public int Duration => _duration;
+            public int[] Review => _review.ToArray();
+            public Movie(string name, int duration)
+            {
+                _name = name;
+                _duration = duration;
+                _review = new int[0];
+            }
+            public void AddReview(int stars)
+            {
+                Array.Resize(ref _review, _review.Length+1);
+                _review[_review.Length-1] = stars;
+            }
+        }
+
+        public class MovieDTO
+        {
+            // свойства с публичными сеттарами
+            public string MovieType {  get; set; }
+            public string Name { get; set; }
+            public int Duration { get; set; }
+            public int[] Review { get; set; }
+            // конструктор без параметров
+            public MovieDTO()
+            {
+
+            }
+            // Movie => MpvieDTO
+            //public MovieDTO(string name, int duration)
+            //{
+            //    Name = name;
+            //    Duration = duration;
+            //}
+
+            public MovieDTO(Movie movie)// movie.GetType() = typeof(Movie)
+                                        // movie.GetType().Name = nameof(Movie)
+            {
+                MovieType = movie.GetType().Name;
+                Name = movie.Name;
+                Duration = movie.Duration;
+                Review = movie.Review;
+            }
+        }
+
         static void Main(string[] args)
         {
+            Movie moviel = new Movie("Harry Potter", 120);
+            moviel.AddReview(5);
+            moviel.AddReview(3);
+            moviel.AddReview(2);
+            moviel.AddReview(4);
+
+            MovieDTO movielDTOl = new MovieDTO(moviel);
             string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            Console.WriteLine(folderPath);
+            string filePath = Path.Combine(folderPath, "movie.xml");
 
-
-            string fileName = "example.txt";
-
-            string fullPath = Path.Combine(folderPath, fileName);
-            Console.WriteLine(fullPath);
-
-            File.Create(fullPath).Close();
-            File.WriteAllText(fullPath, "HEllo" + Environment.NewLine);
-            File.AppendAllText(fullPath, "WOOORLD!");
-            File.WriteAllText(fullPath, "hi, world!");
-
-            string[] words = new string[] { "aaa", "bbb", "ccc" };
-            File.WriteAllLines(fullPath, words);
-            File.AppendAllLines(fullPath, words);
-            File.AppendAllText(fullPath, "slovo");
-
-            string content = File.ReadAllText(fullPath);
-            string[] lines = File.ReadAllLines(fullPath);
-
-            foreach(string line in lines)
+            var serializer = new XmlSerializer(typeof(MovieDTO));
+            //Серриализация
+            using (var writer  = new StreamWriter(filePath))
             {
-                Console.WriteLine(line);
+                serializer.Serialize(writer, movielDTOl);
+            }
+            //Диссериализация
+            MovieDTO movielDTO2;
+            using ( var reader = new StreamReader(filePath))
+            {
+               movielDTO2 =(MovieDTO)serializer.Deserialize(reader);
             }
 
-            string folderPath2 = Path.Combine(folderPath, "ExampleFolder");
-            string filePath = Path.Combine(folderPath2, "anotherExampleFolder.txt");
-
-            if(!Directory.Exists(folderPath2))
+            Movie moviel2 = new Movie(movielDTO2.Name, movielDTO2.Duration);
+            
+            foreach(int star in movielDTO2.Review)
             {
-                Directory.CreateDirectory(folderPath2);
+                moviel2.AddReview(star);
             }
 
-            string ext = "txt";
-
-            if(!Directory.Exists(filePath))
+            if (CompareMovie(moviel, moviel2))
             {
-                File.Create(filePath).Close();
+                Console.WriteLine( "Success");
             }
             else
             {
-                File.WriteAllText(filePath, "");
+                Console.WriteLine("Not success");
             }
-            
-       
-            //знаем только полный путь
-            string FolderPath3 = Path.GetDirectoryName(filePath);
-            string fileName2 = Path.GetFileName(filePath);
+        }
 
-            Console.WriteLine(fileName2);
-            Console.WriteLine(FolderPath3);
+        public static bool CompareMovie(Movie m1, Movie m2)
+        {
 
-            
+            if (m1.Name != m2.Name) return false;
+            if (m1.Duration != m2.Duration) return false;
+            if (m1.Review != m2.Review) return false;
+            return true;
         }
     }
 }
