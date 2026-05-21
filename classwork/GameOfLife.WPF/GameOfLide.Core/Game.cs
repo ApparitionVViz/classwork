1ab27a61d582cdb3007f4148ffc0b6c0e4880d2e
Lukashev Roman <74509534+ApparitionVViz@users.noreply.github.com>
2026-05-21 18:11:13 +0300

    classwor2105

---
+++ classwork/GameOfLife.WPF/GameOfLide.Core/Game.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLide.Core
+{
+    public class Game
+    {
+        public int Rows { get; }
+        public int Cols { get; }
+
+        public int Generation { get; private set; }
+
+        private bool[,] _grid;
+        public bool[,] Grid => (bool[,])_grid.Clone();
+
+        private bool[,] _nextGrid;
+
+        public Game(int rows = 20, int cols = 30)
+        {
+            Rows = rows;
+            Cols = cols;
+            Generation = 1;
+            _grid = new bool[Rows, Cols];
+            _nextGrid = new bool[Rows, Cols];
+        }
+
+        public void Clean()
+        {
+            Generation = 1;
+            Array.Clear(_grid, 0, _grid.Length);
+        }
+        public void ToggleCell(int row, int col)
+        {
+            if (IsValidCell(row, col)) return;
+
+            _grid[row, col] = !_grid[row, col];
+
+        }
+
+        private bool IsValidCell(int row, int col)
+        {
+            if (row <0 || row >= Rows || col < 0 || col >= Cols)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void NextGeneration()
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                for(int j = 0; j < Cols; j++)
+                {
+                    // посчитать соседей
+                    int n = CountNeighbours(i, j);
+
+                    if (_grid[i, j])
+                    {
+                        if( n==2 || n == 3) _nextGrid[i, j] = true;
+                        else _nextGrid[i, j] = false;
+                    }
+                    else
+                    {
+                        if (n == 3) _nextGrid[i, j] = true;
+                        else _nextGrid[i, j] = false;
+                    }
+
+                }
+            }
+            Array.Copy(_nextGrid, _grid, _grid.Length);
+            Generation += 1;
+        }
+        
+
+
+        private int CountNeighbours(int row, int col)
+        {
+            int count = 0;
+            for(int i = -1; i <= 1; i++)
+            {
+                for(int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 && j == 0) continue;
+
+                    int trgRow = row + i;
+                    int trgCol = col + j;
+                    if (!IsValidCell(trgRow, trgCol)) continue;
+
+                    if (_grid[trgRow, trgCol])
+                        count++;
+                    
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ classwork/GameOfLife.WPF/GameOfLife.WPF/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using GameOfLide.Core;
+namespace GameOfLife.WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Game _game;
+        private Border[,] _cells;
+        private DispatcherTimer _timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _game = new Game();
+            _cells = new Border[_game.Rows, _game.Cols];
+            CreateGameGrid();
+            UpdateVisuals();
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(200)
+            };
+            _timer.Tick += TimerTick;
+        }
+        private void CreateGameGrid()
+        {
+            gameGrid.Children.Clear();
+            gameGrid.Rows = _game.Rows;
+            gameGrid.Columns = _game.Cols;
+            for (int i = 0; i < _game.Rows; i++)
+            {
+                for (int j = 0; j < _game.Cols; j++)
+                {
+                    Border border = new Border
+                    {
+                        Background = Brushes.Black,
+                        BorderBrush = Brushes.DimGray,
+                        BorderThickness = new Thickness(0.3)
+                    };
+
+                    int iCaptured = i, jCaptured = j;
+                    border.MouseLeftButtonDown += (sender, e) => CellClick(iCaptured, jCaptured);
+                    _cells[i, j] = border;
+                    gameGrid.Children.Add(border);
+                }
+            }
+            
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            _game.NextGeneration();
+            UpdateVisuals();
+        }
+
+        private void UpdateVisuals()
+        {
+            for (int i = 0; i < _game.Rows; i++)
+            {
+                for(int j = 0; j < _game.Cols; j++)
+                {
+                    if (_game.Grid[i, j])
+                    {
+                        _cells[i, j].Background = Brushes.Pink;
+                    }
+                    else
+                    {
+                        _cells[i, j].Background = Brushes.Black;
+                    }
+                }
+            }
+        }
+        private void CellClick(int row, int col)
+        {
+            if (_timer.IsEnabled) return;
+
+            _game.ToggleCell(row, col);
+            UpdateVisuals();
+        }
+
+        private void StartClick(object sender, RoutedEventArgs e)
+        {
+            _timer.Start();
+        }
+        private void StopClick(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+        }
+        private void ResetClick(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+            _game.Clean();
+            UpdateVisuals();
+        }
+    }
+}
